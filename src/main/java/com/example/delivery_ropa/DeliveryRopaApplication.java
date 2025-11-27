de12152249e68f5760b2d66f324eca860bc8e885
DiegoSC <u23215658@utp.edu.pe>
2025-11-27 15:03:14 -0500

    VFINAL

---
+++ src/main/java/com/example/delivery_ropa/DeliveryRopaApplication.java
@@ -0,0 +1,13 @@
+package com.example.delivery_ropa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryRopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryRopaApplication.class, args);
+	}
+
+}
